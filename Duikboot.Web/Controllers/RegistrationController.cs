16fa966a009e0aa8226ba488f7d4f463314e8e9a
Thom van de Pas <thomvandepas@hotmail.com>
2018-11-09 18:36:50 +0100

    A lot of progress

--- Duikboot.Web/Controllers/RegistrationController.cs
+++ Duikboot.Web/Controllers/RegistrationController.cs
@@ -7,50 +7,65 @@ using Mollie.Api.Models;
 using System;
 using Mollie.Api.Models.Payment.Response;
 using System.Threading.Tasks;
+using System.Web.Mvc;
+using Duikboot.Web.Models;
 using Mollie.Api.Models.Payment;
 
 namespace Duikboot.Web.Controllers
 {
 
-    public class RegistrationController :ApiController
+    public class RegistrationController : Controller
     {
-        private readonly IPaymentClient paymentClient;
+        private readonly IPaymentClient _paymentClient;
+        private User _user;
 
         public RegistrationController()
         {
-            this.paymentClient = new PaymentClient(ConfigurationManager.AppSettings["MollieTestKey"]);
+            this._paymentClient = new PaymentClient(ConfigurationManager.AppSettings["MollieTestKey"]);
         }
 
-        [HttpGet]
-        public IHttpActionResult Index()
+        [System.Web.Http.HttpGet]
+        public ActionResult Index()
         {
-            return Ok();
+            return View("~/Views/Registration/Register.cshtml");
         }
 
-        [HttpPost]
-        public async Task<IHttpActionResult> Submit([FromBody]Duikboot.Web.Models.User user)
+        [System.Web.Mvc.HttpPost]
+        [System.Web.Mvc.AcceptVerbs(HttpVerbs.Post)]
+        public async Task<ActionResult> Submit([FromBody]Duikboot.Web.Models.User user)
         {
             //Create new payment 
             PaymentRequest paymentRequest = new PaymentRequest()
             {
-                Amount = new Amount(Currency.EUR, "100"),
+                Amount = new Amount(Currency.EUR, "100.00"),
                 Description = $"Betaling carnaval 2019 met CV D'N Duikboot - {user.FirstName} {user.SurName}",
-                RedirectUrl = "https://www.pornhub.com"
+                RedirectUrl = HttpContext.Request.Url.Scheme + "://" + HttpContext.Request.Url.Authority + Url.Action("Complete", "Registration", null)
             };
 
-            var paymentResponse = await paymentClient.CreatePaymentAsync(paymentRequest);
-            var secondPaymentResponse = await paymentClient.GetPaymentAsync(paymentResponse.Id);
+            var paymentResponse = await _paymentClient.CreatePaymentAsync(paymentRequest);
 
-            var test = secondPaymentResponse.Links.Checkout;
+            Session["paymentResponseId"] = paymentResponse.Id;
+            Session["Meerijder"] = user;
 
+            return Redirect(paymentResponse.Links.Checkout.Href);
+
+        }
+
+        [System.Web.Mvc.HttpGet]
+        public async Task<ActionResult> Complete()
+        {
+            var paymentResponseId = Session["paymentResponseId"].ToString();
+            PaymentResponse paymentResponse = await this._paymentClient.GetPaymentAsync(paymentResponseId);
             switch (paymentResponse.Status)
             {
                 case (PaymentStatus.Paid):
-                    return Ok();
-
-                default: return BadRequest(paymentResponse.Status.ToString());
+                    Models.User meerijder = Session["Meerijder"] as Models.User;
+                    // Save _user;
+                    return View("Complete");
+                default:
+                    return View("Failed");
             }
-
         }
+
     }
 }
